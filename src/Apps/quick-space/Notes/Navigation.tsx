cd4e251a862b423ded74998e522a5a14e4b0fff9
Vic van Cooten <duveaux1@gmail.com>
2020-04-16 22:00:49 +0200

    Allowed ugly categorisation for notes

--- src/Apps/quick-space/Notes/Navigation.tsx
+++ src/Apps/quick-space/Notes/Navigation.tsx
@@ -1,37 +1,138 @@
-import React, { useState } from "react";
-import { find } from "lodash";
-import {
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-} from "@material-ui/core";
-import { GiFlexibleStar } from "react-icons/gi";
+// disabling flowtype to keep this example super simple
+// It matches
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import React, { useState, useEffect } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { AppContextType } from "../../../Utils/Types";
+import { findIndex } from "lodash";
 
-const AppQSNotesNavigation: React.FC<{ projects; context: AppContextType }> = ({
-  projects,
-  context,
-}) => {
-  const [project, selectProject] = useState();
-  const [subproject, selectSubproject] = useState();
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
 
-  return (
-    <div style={{ padding: 15 }}>
-      <context.UI.Forms.SelectInput
-        label="Project"
-        options={projects}
-        onChange={(value) => {
-          selectProject(value);
-        }}
-      />
-      {project && (
-        <context.UI.TreeView
-          items={find(projects, (o) => o.value === project).subItems}
-          linkTo={"/quick-space/notes"}
-        />
-      )}
-    </div>
-  );
+  return result;
 };
 
+const grid = 8;
+
+const AppQSNotesNavigation: React.FC<{
+  context: AppContextType;
+  projects;
+  memos;
+}> = ({ context, projects, memos }) => {
+  // Vars
+  const [items, setItems] = useState([
+    { id: "item-1", content: "item 1" },
+    { id: "item-2", content: "item 2" },
+    { id: "item-3", content: "item 3" },
+    { id: "item-4", content: "item 4" },
+  ]);
+  const [projectId, setProjectId] = useState(projects[0].value);
+  const [project, setProject] = useState();
+  const [subprojects, setSubprojects] = useState();
+  const [subprojectId, setSubprojectId] = useState();
+  const [subproject, setSubproject] = useState();
+
+  // Lifecycle
+  useEffect(() => {
+    const index = findIndex(projects, (o) => {
+      return o.value === projectId;
+    });
+    setProject(projects[index]);
+    setSubprojects(projects[index].subprojects);
+    if (projects[index].subprojects) {
+      setSubprojectId(projects[index].subprojects[0].value);
+      setSubproject(
+        projects[index].subprojects[projects[index].subprojects[0].value]
+      );
+    }
+  }, [projectId]);
+
+  // Functions
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const newItems = reorder(
+      items,
+      result.source.index,
+      result.destination.index
+    );
+
+    // @ts-ignore
+    setItems(newItems);
+  };
+
+  // UI
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div style={{ margin: 15 }}>
+        <context.UI.Forms.SelectInput
+          options={projects}
+          value={projectId}
+          onChange={(value) => {
+            setProjectId(value);
+          }}
+        />
+        {subprojects && (
+          <context.UI.Forms.SelectInput
+            options={subprojects}
+            value={subprojectId}
+            onChange={(value) => {
+              setSubprojectId(value);
+            }}
+          />
+        )}
+      </div>
+    </DragDropContext>
+  );
+};
+/*      <Droppable droppableId="droppable">
+        {(droppableProvided, droppableSnapshot) => (
+          <div
+            ref={droppableProvided.innerRef}
+            style={{
+              transition: "all 1s",
+              padding: grid,
+            }}
+          >
+            {project.subProjects.map((item, index) => {
+              return (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(draggableProvided, draggableSnapshot) => (
+                    <div
+                      ref={draggableProvided.innerRef}
+                      {...draggableProvided.draggableProps}
+                      {...draggableProvided.dragHandleProps}
+                      style={{
+                        // some basic styles to make the items look a bit nicer
+                        userSelect: "none",
+                        padding: grid * 2,
+                        margin: `0 0 ${grid}px 0`,
+                        transition: "all 1s",
+                        // change background colour if dragging
+                        background: draggableSnapshot.isDragging
+                          ? "#ffffff"
+                          : "#eeeeee",
+
+                        // styles we need to apply on draggables
+                        ...draggableProvided.draggableProps.style,
+                      }}
+                    >
+                      {item.label}
+                    </div>
+                  )}
+                </Draggable>
+              );
+            })}
+            {droppableProvided.placeholder}
+          </div>
+        )}
+      </Droppable>
+ */
 export default AppQSNotesNavigation;
--- src/Apps/quick-space/Notes/index.tsx
+++ src/Apps/quick-space/Notes/index.tsx
@@ -2,94 +2,70 @@ import React, { useState, useEffect, useGlobal } from "reactn";
 import { AppContextType } from "../../../Utils/Types";
 import { TreeView, TreeItem } from "@material-ui/lab";
 import { filter } from "lodash";
-import AppQSNotesDetail from "./NotesDetail";
 import AppQSNotesNavigation from "./Navigation";
-import { FaStickyNote, FaProjectDiagram, FaTasks } from "react-icons/fa";
+import { Grid } from "@material-ui/core";
 
 const AppQSActionNotes: React.FC<{
   match: { isExact: boolean };
   context: AppContextType;
   action: string;
 }> = ({ context, action, match: { isExact } }) => {
-  // Vars
+  //Vars
   const [projects, setProjects] = useState();
-  const [isMobile] = useGlobal<any>("isMobile");
+  const [memos, setMemos] = useState();
 
   // Lifecycle
   useEffect(() => {
-    context.getObjects("qs-project", {}, (projectResponse) => {
-      context.getObjects("qs-memo", {}, (memoResponse) => {
-        if (projectResponse.success) {
-          if (memoResponse.success) {
-            // Map projects for treeview
-            const newProjects = [];
-            projectResponse.data.map((project) => {
-              if (!project.data.parent) {
-                const subItems = [];
-                // Subprojects
-                filter(projectResponse.data, (o) => {
-                  return o.data.parent === project._id;
-                }).map((subProject) => {
-                  // Subprojects memos
-                  const subMemos = [];
-                  filter(memoResponse.data, (o) => {
-                    return o.data.project === subProject._id;
-                  }).map((subMemo) => {
-                    subMemos.push({
-                      key: subMemo._id,
-                      label: subMemo.data.title,
-                      icon: FaStickyNote,
-                    });
-                  });
-
-                  subItems.push({
-                    key: subProject._id,
-                    label: subProject.data.name,
-                    icon: FaTasks,
-                    subItems: subMemos.length > 0 ? subMemos : undefined,
-                  });
-                });
-                // Submemos
-                filter(memoResponse.data, (o) => {
-                  return o.data.project === project._id;
-                }).map((subProject) => {
-                  subItems.push({
-                    key: subProject._id,
-                    label: subProject.data.title,
-                    icon: FaStickyNote,
-                  });
-                });
-
-                newProjects.push({
-                  value: project._id,
-                  label: project.data.name,
-                  subItems,
-                });
-              }
+    context.getObjects("qs-project", {}, (response) => {
+      if (response.success) {
+        const newProjects = [];
+        response.data.map((project) => {
+          if (!project.data.parent) {
+            const subprojects = [];
+            filter(response.data, (o) => {
+              return o.data.parent === project._id;
+            }).map((subProject) => {
+              subprojects.push({
+                value: subProject._id,
+                label: subProject.data.name,
+              });
             });
-            setProjects(newProjects);
-          } else {
-            console.log(memoResponse);
+            newProjects.push({
+              value: project._id,
+              label: project.data.name,
+              subprojects,
+            });
           }
-        } else {
-          console.log(projectResponse);
-        }
-      });
+        });
+        //@ts-ignore
+        setProjects(newProjects);
+      } else {
+        console.log(response);
+      }
+    });
+
+    context.getObjects("qs-memo", {}, (response) => {
+      if (response.success) {
+        setMemos(response.data);
+      } else {
+        console.log(response);
+      }
     });
   }, []);
 
   // UI
-  if (!projects) return <context.UI.Loading />;
 
+  if (!projects || !memos) return <context.UI.Loading />;
   return (
-    <context.UI.ListDetailLayout
-      baseUrl="quick-space/notes"
-      customNavComponent={
-        <AppQSNotesNavigation context={context} projects={projects} />
-      }
-      DetailComponent={AppQSNotesDetail}
-      context={context}
-    />
+    <Grid container>
+      <Grid item xs={3}>
+        <AppQSNotesNavigation
+          memos={memos}
+          projects={projects}
+          context={context}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
