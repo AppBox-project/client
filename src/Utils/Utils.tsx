06f5faa68700aa5aca961bc2a1bce0bf1a4320f4
Vic van Cooten <duveaux1@gmail.com>
2020-05-09 13:38:13 +0200

    shoddy fix

--- src/Utils/Utils.tsx
+++ src/Utils/Utils.tsx
@@ -1,5 +1,3 @@
 export const baseUrl = window.location.href.match("localhost")
   ? "http://localhost:8600"
-  : process.env.srvUrl
-  ? process.env.srvUrl
-  : "/";
+  : "https://appbox.vicvan.co";
