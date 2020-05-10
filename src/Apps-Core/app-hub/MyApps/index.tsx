2f8de78877b1e1b2b99b0454240956a09215ec7e
Vic van Cooten <duveaux1@gmail.com>
2020-05-10 16:38:41 +0200

    Add installed app overview to AH

--- src/Apps-Core/app-hub/MyApps/index.tsx
+++ src/Apps-Core/app-hub/MyApps/index.tsx
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AppContextType } from "../../../Utils/Types";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core";
+import { FaCloudDownloadAlt } from "react-icons/fa";
 
 const AppAHMyApps: React.FC<{
   match: { isExact: boolean };
   context: AppContextType;
   action: string;
 }> = ({ context, action, match: { isExact } }) => {
-  return <>My Apps</>;
+  // Vars
+  const [apps, setApps] = useState();
+
+  // Lifecycle
+  useEffect(() => {
+    context.getObjects("app", { "data.core": { $ne: true } }, (response) => {
+      if (response.success) {
+        setApps(response.data);
+      } else {
+        console.log(response);
+      }
+    });
+  }, []);
+
+  // UI
+  if (!apps) return <context.UI.Loading />;
+  return (
+    <List>
+      {apps.map((app) => {
+        return (
+          <ListItem key={app._id}>
+            <ListItemText>{app.data.name}</ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton
+                color="primary"
+                onClick={() => {
+                  console.log("Todo: update");
+                }}
+              >
+                <FaCloudDownloadAlt />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 };
 
 export default AppAHMyApps;
