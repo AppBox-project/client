ea3c475dff2a7437d660fd24f51c41d48c8f78de
Vic van Cooten <duveaux1@gmail.com>
2020-05-12 19:50:09 +0200

    Remove quill

--- src/Components/Inputs/Drafting/index.tsx
+++ src/Components/Inputs/Drafting/index.tsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import "react-quill/dist/quill.bubble.css";
 
 const InputDrafting: React.FC<{
   placeholder?: string;
@@ -18,33 +15,7 @@ const InputDrafting: React.FC<{
   }, [value]);
 
   // UI
-  return (
-    <>
-      <ReactQuill
-        theme={!mode || mode == "normal" ? "snow" : "bubble"}
-        value={newValue}
-        onChange={(val) => {
-          setNewValue(val);
-          onChange(val);
-        }}
-        placeholder={placeholder}
-        modules={{
-          toolbar: [
-            ["bold", "italic", "underline", "strike"], // toggled buttons
-            [{ align: [] }],
-            ['link', 'image'],
-            [{ header: 1 }, { header: 2 }], // custom button values
-            [{ list: "bullet" }, { list: "ordered" }],
-            [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-            [{ header: [1, 2, 3, 4, 5, 6, false] }],
-            [{ color: [] }], // dropdown with defaults from theme
-            ["blockquote", "code-block"],
-            ["clean"], // remove formatting button
-          ],
-        }}
-      />
-    </>
-  );
+  return <>quill is gone</>;
 };
 
 export default InputDrafting;
