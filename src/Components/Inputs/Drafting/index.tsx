6351b859fbe21af09ef8915704962c06608eae8c
Vic van Cooten <duveaux1@gmail.com>
2020-05-16 10:51:19 +0200

    Add CKEditor

--- src/Components/Inputs/Drafting/index.tsx
+++ src/Components/Inputs/Drafting/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import CKEditor from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const InputDrafting: React.FC<{
   placeholder?: string;
@@ -15,7 +17,26 @@ const InputDrafting: React.FC<{
   }, [value]);
 
   // UI
-  return <>quill is gone</>;
+  return (
+    <CKEditor
+      editor={ClassicEditor}
+      data="<p>Hello from CKEditor 5!</p>"
+      onInit={(editor) => {
+        // You can store the "editor" and use when it is needed.
+        console.log("Editor is ready to use!", editor);
+      }}
+      onChange={(event, editor) => {
+        const data = editor.getData();
+        console.log({ event, editor, data });
+      }}
+      onBlur={(event, editor) => {
+        console.log("Blur.", editor);
+      }}
+      onFocus={(event, editor) => {
+        console.log("Focus.", editor);
+      }}
+    />
+  );
 };
 
 export default InputDrafting;
