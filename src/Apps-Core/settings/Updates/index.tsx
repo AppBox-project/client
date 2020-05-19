7e843b1775fdf6052dfae70d181f662e6595ec14
Vic van Cooten <duveaux1@gmail.com>
2020-05-19 17:32:22 +0200

    Improve upgrade process

--- src/Apps-Core/settings/Updates/index.tsx
+++ src/Apps-Core/settings/Updates/index.tsx
@@ -1,6 +1,6 @@
 import React from "reactn";
 import { AppContextType } from "../../../Utils/Types";
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
 import { useState } from "react";
 
 const AppSettingsUpdate: React.FC<{
@@ -9,35 +9,57 @@ const AppSettingsUpdate: React.FC<{
   action: string;
 }> = ({ context, action, match: { isExact } }) => {
   // Vars
-  const [isUpgrading, setIsUpgrading] = useState(false);
+  const [upgradeTask, setUpgradeTask] = useState();
+
   // Lifecycle
+
   // UI
   return (
-    <Button
-      disabled={isUpgrading}
-      variant="contained"
-      color="primary"
-      onClick={() => {
-        setIsUpgrading(true);
-        context.addObject(
-          "system-task",
-          {
-            type: "Box update",
-            name: `Update box software`,
-            description: `Triggered manually`,
-            when: "asap",
-            action: "box-update",
-            done: false,
-            arguments: undefined,
-          },
-          (response) => {
-            console.log(response);
-          }
-        );
-      }}
-    >
-      Start upgrade
-    </Button>
+    <div className="center" style={{ textAlign: "center" }}>
+      {upgradeTask && (
+        <>
+          <Typography>Upgrading!</Typography>
+          <Typography>
+            {upgradeTask.data.done ? (
+              <>Upgrade done</>
+            ) : (
+              upgradeTask.data.progress + "%"
+            )}
+          </Typography>
+        </>
+      )}
+      <Button
+        disabled={upgradeTask !== undefined}
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          context.addObject(
+            "system-task",
+            {
+              type: "Box update",
+              name: `Update box software`,
+              description: `Triggered manually`,
+              when: "asap",
+              action: "box-update",
+              done: false,
+              arguments: undefined,
+              progress: 0,
+            },
+            (response) => {
+              context.getObjects(
+                "system-task",
+                { _id: response.data._id },
+                (response) => {
+                  setUpgradeTask(response.data[0]);
+                }
+              );
+            }
+          );
+        }}
+      >
+        Start upgrade
+      </Button>
+    </div>
   );
 };
 
