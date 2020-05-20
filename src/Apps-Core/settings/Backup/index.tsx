d43ef37f4b30703ced0af4ec9164309d834c5e70
Vic van Cooten <duveaux1@gmail.com>
2020-05-20 21:25:28 +0200

    UI for backup

--- src/Apps-Core/settings/Backup/index.tsx
+++ src/Apps-Core/settings/Backup/index.tsx
@@ -9,13 +9,57 @@ const AppSettingsBackup: React.FC<{
   action: string;
 }> = ({ context, action, match: { isExact } }) => {
   // Vars
+  const [task, setTask] = useState();
 
   // Lifecycle
 
   // UI
   return (
-    <div className="center">
-      TODO: Make a database upgrade tool, export mongoDB
+    <div className="center" style={{ textAlign: "center" }}>
+      {task ? (
+        <>
+          <Typography variant="h6">{task.data.state}</Typography>
+          <Typography>
+            {task.data.done ? <>Backup done</> : task.data.progress + "%"}
+          </Typography>
+        </>
+      ) : (
+        <>
+          <Typography variant="h6">Back up AppBox</Typography>
+          <Button
+            disabled={task !== undefined}
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              context.addObject(
+                "system-task",
+                {
+                  type: "Database export",
+                  name: `Backing up database`,
+                  description: `Triggered manually`,
+                  when: "asap",
+                  action: "database-backup",
+                  done: false,
+                  arguments: undefined,
+                  progress: 0,
+                  state: "Starting",
+                },
+                (response) => {
+                  context.getObjects(
+                    "system-task",
+                    { _id: response.data._id },
+                    (response) => {
+                      setTask(response.data[0]);
+                    }
+                  );
+                }
+              );
+            }}
+          >
+            Create backup
+          </Button>
+        </>
+      )}
     </div>
   );
 };
