b3310f585086f8a661e3bd4b70c83cf7f72e86ac
Vic van Cooten <duveaux1@gmail.com>
2020-05-21 15:32:16 +0200

    QS: make todo header sticky

--- src/Apps-Core/quick-space/Todo/DetailComponent.tsx
+++ src/Apps-Core/quick-space/Todo/DetailComponent.tsx
@@ -13,6 +13,8 @@ import {
   ListItemIcon,
   ListItemSecondaryAction,
   Chip,
+  ListSubheader,
+  List,
 } from "@material-ui/core";
 import { FaTrello, FaBars, FaAngleDown, FaStickyNote } from "react-icons/fa";
 import { GoTasklist } from "react-icons/go";
@@ -143,220 +145,225 @@ const AppQSActionTodoDetail: React.FC<{
         />
       )}
       {view === "todo" && (
-        <context.UI.Animations.AnimationContainer>
-          <Grid container>
-            <Grid
-              item
-              xs={12}
-              md={8}
-              style={{ padding: 15, boxSizing: "border-box" }}
-            >
-              <context.UI.Animations.AnimationItem>
-                <TextField
-                  fullWidth
-                  margin="normal"
-                  label="Add todo"
-                  value={newTodo}
-                  style={{ margin: "0 15px", width: "95%" }}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      context.addObject(
-                        "qs-todo",
-                        {
-                          action: newTodo,
-                          project: detailId,
-                        },
-                        (response) => {
-                          console.log(response);
-                        }
-                      );
-
-                      setNewTodo("");
-                    }
-                  }}
-                  onChange={(e) => {
-                    setNewTodo(e.target.value);
-                  }}
-                />
-                <context.UI.Layouts.SortableList
-                  listItems={todos}
-                  linkToPath="_id"
-                  baseUrl={`/quick-space/todo/${detailId}`}
-                  customItem={(todo) => {
-                    return (
-                      <ListItem
-                        key={todo._id}
-                        style={{ cursor: "pointer" }}
-                        button
-                      >
-                        <ListItemIcon>
-                          <context.UI.Field
-                            field={model.fields["done"]}
-                            fieldId="done"
-                            objectId={todo._id}
-                            object={todo}
-                            mode="free"
-                            directSave
-                            directSaveDelay={1}
-                          />
-                        </ListItemIcon>
-                        <ListItemText
-                          onClick={(object) => {
-                            context.setDialog({
-                              display: true,
-                              size: "md",
-                              title: todo.data.action,
-                              content: (
-                                <context.UI.Layouts.Object.ObjectLayout
-                                  model={model}
-                                  layoutId="popup"
-                                  popup
-                                  appId="quick-space"
-                                  objectId={todo._id}
-                                />
-                              ),
-                            });
-                          }}
-                          primary={todo.data.action}
-                          secondary={
-                            <>
-                              {todo.data.description && (
-                                <>
-                                  {todo.data.description}
-                                  <br />
-                                </>
-                              )}
-                              {todo.data.status && isMobile && (
-                                <>
-                                  <Chip
-                                    variant="outlined"
-                                    label={todo.data.status}
-                                    size="small"
-                                  />{" "}
-                                </>
-                              )}
-                              {todo.data.tags && (
-                                <context.UI.FieldDisplay
-                                  objectField={todo.data.tags}
-                                  modelField={model.fields.tags}
-                                  props={{ size: "small" }}
-                                />
-                              )}
-                              {todo.data.relatedNotes &&
-                                todo.data.relatedNotes.length > 0 && (
-                                  <>
-                                    {" "}
-                                    <Chip
-                                      label={`${
-                                        todo.data.relatedNotes.length
-                                      } ${
-                                        todo.data.relatedNotes.length === 1
-                                          ? "note"
-                                          : "notes"
-                                      }`}
-                                      icon={
-                                        <FaStickyNote
-                                          style={{ width: 12, height: 12 }}
-                                        />
-                                      }
-                                      size="small"
-                                      variant="outlined"
-                                    />
-                                  </>
-                                )}
-                              {todo.data.relates_to &&
-                                todo.data.relates_to.length > 0 && (
-                                  <>
-                                    {" "}
-                                    <Chip
-                                      label={`${todo.data.relates_to.length} related`}
-                                      icon={
-                                        <GoTasklist
-                                          style={{ width: 12, height: 12 }}
-                                        />
-                                      }
-                                      size="small"
-                                      variant="outlined"
-                                    />
-                                  </>
-                                )}
-                            </>
-                          }
-                        />
-                        {(todo.data.status || todo.data.tags) && !isMobile && (
-                          <ListItemSecondaryAction
-                            style={{ textAlign: "right" }}
-                          >
-                            {todo.data.status && (
-                              <Chip
-                                variant="outlined"
-                                label={todo.data.status}
-                                size="small"
-                              />
-                            )}
-                          </ListItemSecondaryAction>
-                        )}
-                      </ListItem>
-                    );
-                  }}
-                />
-              </context.UI.Animations.AnimationItem>
-            </Grid>
-
-            {doneTodos && doneTodos.length > 0 && (
-              <Grid item xs={12} md={4} style={{ padding: 15 }}>
+        <List>
+          <context.UI.Animations.AnimationContainer>
+            <Grid container>
+              <Grid
+                item
+                xs={12}
+                md={8}
+                style={{ padding: 15, boxSizing: "border-box" }}
+              >
                 <context.UI.Animations.AnimationItem>
-                  <ExpansionPanel>
-                    <ExpansionPanelSummary
-                      expandIcon={<FaAngleDown />}
-                      aria-controls="panel1a-content"
-                      id="panel1a-header"
-                    >
-                      <Typography variant="h6">Done</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                      <context.UI.Layouts.SortableList
-                        listItems={doneTodos}
-                        linkToPath="_id"
-                        listTextPath="data.action"
-                        baseUrl={`/quick-space/todo/${detailId}`}
-                        listSubTextPath="data.description"
-                        onListItemClick={(object) => {
-                          context.setDialog({
-                            display: true,
-                            title: object.data.action,
-                            content: (
-                              <context.UI.Layouts.Object.ObjectLayout
-                                model={model}
-                                layoutId="default"
-                                popup
-                                appId="quick-space"
-                                objectId={object._id}
-                              />
-                            ),
-                          });
-                        }}
-                        listAction={(id, object) => {
-                          return (
+                  <ListSubheader style={{ backgroundColor: "white" }}>
+                    <TextField
+                      fullWidth
+                      margin="normal"
+                      label="Add todo"
+                      value={newTodo}
+                      style={{ margin: "0 15px", width: "98%" }}
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                          context.addObject(
+                            "qs-todo",
+                            {
+                              action: newTodo,
+                              project: detailId,
+                            },
+                            (response) => {
+                              console.log(response);
+                            }
+                          );
+
+                          setNewTodo("");
+                        }
+                      }}
+                      onChange={(e) => {
+                        setNewTodo(e.target.value);
+                      }}
+                    />
+                  </ListSubheader>
+                  <context.UI.Layouts.SortableList
+                    listItems={todos}
+                    linkToPath="_id"
+                    baseUrl={`/quick-space/todo/${detailId}`}
+                    customItem={(todo) => {
+                      return (
+                        <ListItem
+                          key={todo._id}
+                          style={{ cursor: "pointer" }}
+                          button
+                        >
+                          <ListItemIcon>
                             <context.UI.Field
                               field={model.fields["done"]}
                               fieldId="done"
-                              objectId={id}
-                              object={object}
+                              objectId={todo._id}
+                              object={todo}
                               mode="free"
                               directSave
                               directSaveDelay={1}
                             />
-                          );
-                        }}
-                      />{" "}
-                    </ExpansionPanelDetails>
-                  </ExpansionPanel>
+                          </ListItemIcon>
+                          <ListItemText
+                            onClick={(object) => {
+                              context.setDialog({
+                                display: true,
+                                size: "md",
+                                title: todo.data.action,
+                                content: (
+                                  <context.UI.Layouts.Object.ObjectLayout
+                                    model={model}
+                                    layoutId="popup"
+                                    popup
+                                    appId="quick-space"
+                                    objectId={todo._id}
+                                  />
+                                ),
+                              });
+                            }}
+                            primary={todo.data.action}
+                            secondary={
+                              <>
+                                {todo.data.description && (
+                                  <>
+                                    {todo.data.description}
+                                    <br />
+                                  </>
+                                )}
+                                {todo.data.status && isMobile && (
+                                  <>
+                                    <Chip
+                                      variant="outlined"
+                                      label={todo.data.status}
+                                      size="small"
+                                    />{" "}
+                                  </>
+                                )}
+                                {todo.data.tags && (
+                                  <context.UI.FieldDisplay
+                                    objectField={todo.data.tags}
+                                    modelField={model.fields.tags}
+                                    props={{ size: "small" }}
+                                  />
+                                )}
+                                {todo.data.relatedNotes &&
+                                  todo.data.relatedNotes.length > 0 && (
+                                    <>
+                                      {" "}
+                                      <Chip
+                                        label={`${
+                                          todo.data.relatedNotes.length
+                                        } ${
+                                          todo.data.relatedNotes.length === 1
+                                            ? "note"
+                                            : "notes"
+                                        }`}
+                                        icon={
+                                          <FaStickyNote
+                                            style={{ width: 12, height: 12 }}
+                                          />
+                                        }
+                                        size="small"
+                                        variant="outlined"
+                                      />
+                                    </>
+                                  )}
+                                {todo.data.relates_to &&
+                                  todo.data.relates_to.length > 0 && (
+                                    <>
+                                      {" "}
+                                      <Chip
+                                        label={`${todo.data.relates_to.length} related`}
+                                        icon={
+                                          <GoTasklist
+                                            style={{ width: 12, height: 12 }}
+                                          />
+                                        }
+                                        size="small"
+                                        variant="outlined"
+                                      />
+                                    </>
+                                  )}
+                              </>
+                            }
+                          />
+                          {(todo.data.status || todo.data.tags) &&
+                            !isMobile && (
+                              <ListItemSecondaryAction
+                                style={{ textAlign: "right" }}
+                              >
+                                {todo.data.status && (
+                                  <Chip
+                                    variant="outlined"
+                                    label={todo.data.status}
+                                    size="small"
+                                  />
+                                )}
+                              </ListItemSecondaryAction>
+                            )}
+                        </ListItem>
+                      );
+                    }}
+                  />
                 </context.UI.Animations.AnimationItem>
               </Grid>
-            )}
-          </Grid>
-        </context.UI.Animations.AnimationContainer>
+
+              {doneTodos && doneTodos.length > 0 && (
+                <Grid item xs={12} md={4} style={{ padding: 15 }}>
+                  <context.UI.Animations.AnimationItem>
+                    <ExpansionPanel>
+                      <ExpansionPanelSummary
+                        expandIcon={<FaAngleDown />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                      >
+                        <Typography variant="h6">Done</Typography>
+                      </ExpansionPanelSummary>
+                      <ExpansionPanelDetails>
+                        <context.UI.Layouts.SortableList
+                          listItems={doneTodos}
+                          linkToPath="_id"
+                          listTextPath="data.action"
+                          baseUrl={`/quick-space/todo/${detailId}`}
+                          listSubTextPath="data.description"
+                          onListItemClick={(object) => {
+                            context.setDialog({
+                              display: true,
+                              title: object.data.action,
+                              content: (
+                                <context.UI.Layouts.Object.ObjectLayout
+                                  model={model}
+                                  layoutId="default"
+                                  popup
+                                  appId="quick-space"
+                                  objectId={object._id}
+                                />
+                              ),
+                            });
+                          }}
+                          listAction={(id, object) => {
+                            return (
+                              <context.UI.Field
+                                field={model.fields["done"]}
+                                fieldId="done"
+                                objectId={id}
+                                object={object}
+                                mode="free"
+                                directSave
+                                directSaveDelay={1}
+                              />
+                            );
+                          }}
+                        />{" "}
+                      </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                  </context.UI.Animations.AnimationItem>
+                </Grid>
+              )}
+            </Grid>
+          </context.UI.Animations.AnimationContainer>
+        </List>
       )}
     </>
   );
