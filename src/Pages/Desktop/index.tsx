700038a14843faaec2acf73d862033a6c58900d7
Vic van Cooten <duveaux1@gmail.com>
2020-08-14 13:20:36 +0200

    Fix overflowing app bar on low heights

--- src/Pages/Desktop/index.tsx
+++ src/Pages/Desktop/index.tsx
@@ -2,7 +2,6 @@ import React, { useGlobal } from "reactn";
 import styles from "./styles.module.scss";
 import { motion } from "framer-motion";
 import {
-  Grid,
   IconButton,
   Avatar,
   CircularProgress,
@@ -118,81 +117,66 @@ const AppBar: React.FC<{ currentApp: string }> = ({ currentApp }) => {
       }}
     >
       <div className={styles.shadow} />
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        style={{ height: "100%" }}
+      <motion.div variants={item} style={{ width: "100%", height: 64 }}>
+        <Link to="/">
+          <Tooltip title="Home" placement="right">
+            <IconButton style={{ color: "white" }}>
+              <icons.FaBoxOpen style={{ width: 40, height: 40 }} />
+            </IconButton>
+          </Tooltip>
+        </Link>
+      </motion.div>
+      <div
+        style={{
+          flex: 1,
+          position: "relative",
+          zIndex: 501,
+          overflow: "hidden",
+        }}
       >
-        <Grid item xs={1} className={styles.item}>
-          <motion.div variants={item} style={{ width: "100%" }}>
-            <Link to="/">
-              <Tooltip title="Home" placement="right">
-                <IconButton style={{ color: "white" }}>
-                  <icons.FaBoxOpen style={{ width: 40, height: 40 }} />
-                </IconButton>
-              </Tooltip>
-            </Link>
-          </motion.div>
-        </Grid>
-        <Grid item xs={10} className={styles.item}>
-          <Grid container direction="column" style={{ height: "100%" }}>
-            {apps.map((app) => {
-              const Icon = icons[app.data.icon];
-              return (
-                <Grid
-                  item
-                  xs={1}
-                  className={`${styles.item} ${
-                    currentApp === app.data.id && styles.active
-                  }`}
-                  key={app._id}
-                >
-                  <motion.div variants={item} style={{ width: 64 }}>
-                    <Tooltip placement="right" title={app.data.name}>
-                      <Link to={`/${app.data.id}`} className="no-link">
-                        <IconButton>
-                          <Avatar
-                            variant="rounded"
-                            style={{
-                              transition: "all 1s",
-                              backgroundColor: `rgb(${app.data.color.r},${app.data.color.g},${app.data.color.b})`,
-                            }}
-                          >
-                            <Icon />
-                          </Avatar>
-                        </IconButton>
-                      </Link>
-                    </Tooltip>
-                  </motion.div>
-                </Grid>
-              );
-            })}
-          </Grid>
-        </Grid>
-
-        <Grid
-          item
-          xs={1}
-          className={styles.item}
-          style={{ alignItems: "flex-end" }}
-        >
-          <motion.div variants={item}>
-            <Link to={`/o/${user._id}`}>
-              <Tooltip placement="right" title={`Hi ${user.data.first_name}`}>
-                <IconButton style={{ width: 64 }}>
-                  {user.data.picture ? (
-                    <Avatar src={baseUrl + user.data.picture} />
-                  ) : (
-                    <Avatar>{user.data.first_name}</Avatar>
-                  )}
-                </IconButton>
-              </Tooltip>
-            </Link>
-          </motion.div>
-        </Grid>
-      </Grid>
+        {apps.map((app) => {
+          const Icon = icons[app.data.icon];
+          return (
+            <div
+              className={`${styles.item} ${
+                currentApp === app.data.id && styles.active
+              }`}
+              key={app._id}
+            >
+              <motion.div variants={item} style={{ width: 64 }}>
+                <Tooltip placement="right" title={app.data.name}>
+                  <Link to={`/${app.data.id}`} className="no-link">
+                    <IconButton>
+                      <Avatar
+                        variant="rounded"
+                        style={{
+                          transition: "all 1s",
+                          backgroundColor: `rgb(${app.data.color.r},${app.data.color.g},${app.data.color.b})`,
+                        }}
+                      >
+                        <Icon />
+                      </Avatar>
+                    </IconButton>
+                  </Link>
+                </Tooltip>
+              </motion.div>
+            </div>
+          );
+        })}
+      </div>
+      <motion.div variants={item} style={{ height: 64, zIndex: 502 }}>
+        <Link to={`/o/${user._id}`}>
+          <Tooltip placement="right" title={`Hi ${user.data.first_name}`}>
+            <IconButton style={{ width: 64 }}>
+              {user.data.picture ? (
+                <Avatar src={baseUrl + user.data.picture} />
+              ) : (
+                <Avatar>{user.data.first_name}</Avatar>
+              )}
+            </IconButton>
+          </Tooltip>
+        </Link>
+      </motion.div>
     </motion.div>
   );
 };
