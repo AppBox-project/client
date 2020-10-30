4d8ac78f034fb4afd5c29d2c94c9982656e5f5cf
Vic van Cooten <duveaux1@gmail.com>
2020-10-30 23:06:56 +0100

    Add cronOptions

--- src/Apps-Core/settings/Automations/ProcessEditor.tsx
+++ src/Apps-Core/settings/Automations/ProcessEditor.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "reactn";
 import { AppContextType } from "../../../Utils/Types";
 import { AutomationType, ProcessStepAction } from "../Types";
 import styles from "./styles.module.scss";
+import { find } from "lodash";
 
 const ProcessEditor: React.FC<{
   context: AppContextType;
@@ -13,6 +14,30 @@ const ProcessEditor: React.FC<{
   const [newAutomation, setNewAutomation] = useState<AutomationType>(
     automation
   );
+  const cronOptions = [
+    { label: "Every minute", value: "minutely", cron: "* * * * *" },
+    { label: "Every hour", value: "hourly", cron: "0 * * * *" },
+    { label: "Every other hour", value: "hourly", cron: "0 */2 * * *" },
+    { label: "Every day", value: "daily", cron: "0 0 * * *" },
+    { label: "Daily (at 1am)", value: "daily_1", cron: "0 1 * * *" },
+    { label: "Daily (at 2am)", value: "daily_2", cron: "0 2 * * *" },
+    { label: "Daily (at 8am)", value: "daily_8", cron: "0 8 * * *" },
+    { label: "On mondays", value: "mondays", cron: "0 0 * * MON" },
+    { label: "On tuesdays", value: "tuesdays", cron: "0 0 * * TUE" },
+    { label: "On wednesdays", value: "wednesdays", cron: "0 0 * * WED" },
+    { label: "On thursdays", value: "thursdays", cron: "0 0 * * THU" },
+    { label: "On fridays", value: "fridays", cron: "0 0 * * FRI" },
+    { label: "On saturdays", value: "saturdays", cron: "0 0 * * SAT" },
+    { label: "On sundays", value: "sundays", cron: "0 0 * * SUN" },
+    { label: "On weekdays", value: "weekdays", cron: "0 0 * * 1-5" },
+    { label: "On the weekend", value: "weekend", cron: "0 0 * * 6,0" },
+    { label: "Every week", value: "weekly", cron: "0 0 * * 0" },
+    { label: "Every month", value: "monthly", cron: "0 0 1 * *" },
+    { label: "Every quarter", value: "quarterly", cron: "0 0 1 */3 *" },
+    { label: "Twice a year", value: "twice_yearly", cron: "0 0 1 */6 *" },
+    { label: "Every year", value: "yearly", cron: "0 0 1 1 *" },
+    { label: "Date / Time", value: "cron" },
+  ];
 
   // Lifecycle
   useEffect(() => {
@@ -82,7 +107,40 @@ const ProcessEditor: React.FC<{
           {(newAutomation.data?.data?.triggers || []).length > 0 ? (
             <>
               {newAutomation.data.data.triggers.map((trigger, index) => (
-                <div key={index} className={styles.trigger}>
+                <div
+                  key={index}
+                  className={styles.trigger}
+                  onClick={() => {
+                    context.setDialog({
+                      display: true,
+                      title: "Edit trigger",
+                      form: [
+                        {
+                          label: "Trigger",
+                          key: "preset",
+                          type: "dropdown",
+                          dropdownOptions: cronOptions,
+                        },
+                      ],
+                      buttons: [
+                        {
+                          label: "Update",
+                          onClick: (form) => {
+                            if (form.preset === "cron") {
+                              console.log("custom cron");
+                            } else {
+                              const cron = find(
+                                cronOptions,
+                                (o) => o.value === form.preset
+                              ).cron;
+                              console.log(cron);
+                            }
+                          },
+                        },
+                      ],
+                    });
+                  }}
+                >
                   <div className={styles.triggerTitle}>Every night</div>
                   {trigger.type}
                 </div>
