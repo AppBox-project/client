63756360129ba8cf283cadf5f30735ea9751be99
Vic van Cooten <duveaux1@gmail.com>
2020-11-18 17:44:16 +0100

    Fix button not showing

--- src/Apps-Core/model-manager/Actions/ManageObject/Tabs/Actions/Detail.tsx
+++ src/Apps-Core/model-manager/Actions/ManageObject/Tabs/Actions/Detail.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { AppContextType, ModelType } from "../../../../../../Utils/Types";
-import { Button } from "@material-ui/core";
+import { Button, Fab } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
+import { FaSave } from "react-icons/fa";
 
 const AppActionManageObjectActionsDetail: React.FC<{
   match: { params: { detailId } };
@@ -14,66 +15,68 @@ const AppActionManageObjectActionsDetail: React.FC<{
   context,
   model,
 }) => {
-  // Vars
-  const [action, setAction] = useState<any>();
+    // Vars
+    const [action, setAction] = useState<any>();
+    const [hasChanged, setHasChanged] = useState<boolean>(false);
 
-  // Lifecycle
-  useEffect(() => {
-    setAction(model.actions[detailId]);
-  }, [detailId]);
+    // Lifecycle
+    useEffect(() => {
+      setAction(model.actions[detailId]);
+    }, [detailId]);
 
-  // UI
-  return (
-    <context.UI.Animations.AnimationContainer>
-      <context.UI.Animations.AnimationItem>
-        <context.UI.Design.Card withBigMargin title="Action">
-          {!action ? (
-            <>
-              <Skeleton variant="text" />
-            </>
-          ) : (
-            <>
-              <context.UI.Inputs.TextInput
-                label="Lay-out"
-                value={action.layout}
-                onChange={(value) => {
-                  setAction({ ...action, layout: value });
-                }}
-              />{" "}
-              <context.UI.Inputs.Select
-                label="Type"
-                options={[{ value: "create", label: "create" }]}
-                value={action.type}
-                onChange={(value) => {
-                  setAction({ ...action, type: value });
-                }}
-              />
-            </>
+    // UI
+    return (
+      <context.UI.Animations.AnimationContainer>
+        <context.UI.Animations.AnimationItem>
+          <context.UI.Design.Card withBigMargin title="Action">
+            {!action ? (
+              <>
+                <Skeleton variant="text" />
+              </>
+            ) : (
+                <>
+                  <context.UI.Inputs.TextInput
+                    label="Lay-out"
+                    value={action.layout}
+                    onChange={(value) => {
+                      setAction({ ...action, layout: value });
+                      setHasChanged(true);
+                    }}
+                  />{" "}
+                  <context.UI.Inputs.Select
+                    label="Type"
+                    options={[{ value: "create", label: "create" }]}
+                    value={action.type}
+                    onChange={(value) => {
+                      setAction({ ...action, type: value });
+                      setHasChanged(true);
+                    }}
+                  />
+                </>
+              )}
+          </context.UI.Design.Card>
+        </context.UI.Animations.AnimationItem>
+        <context.UI.Animations.AnimationItem>
+          {hasChanged && (
+            <Fab style={{ position: 'fixed', bottom: 15, right: 15 }}
+              color="primary"
+              onClick={() => {
+                context.updateModel(
+                  model.key,
+                  {
+                    ...model,
+                    actions: { ...model.actions, [detailId]: action },
+                  },
+                  model._id
+                );
+              }}
+            >
+              <FaSave />
+            </Fab>
           )}
-        </context.UI.Design.Card>
-      </context.UI.Animations.AnimationItem>
-      <context.UI.Animations.AnimationItem>
-        {action !== model.actions[detailId] && (
-          <Button
-            fullWidth
-            color="primary"
-            onClick={() => {
-              context.updateModel(
-                model.key,
-                {
-                  ...model,
-                  actions: { ...model.actions, [detailId]: action },
-                },
-                model._id
-              );
-            }}
-          >
-            Save
-          </Button>
-        )}
-      </context.UI.Animations.AnimationItem>
-    </context.UI.Animations.AnimationContainer>
-  );
-};
+        </context.UI.Animations.AnimationItem>
+      </context.UI.Animations.AnimationContainer>
+    );
+  };
 
 export default AppActionManageObjectActionsDetail;
