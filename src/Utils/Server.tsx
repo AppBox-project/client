e1499bde65197ccf60d493341af28a0ec8f02cf3
Vic van Cooten <duveaux1@gmail.com>
2020-12-04 15:11:19 +0100

    Set CORS on socket

--- src/Utils/Server.tsx
+++ src/Utils/Server.tsx
@@ -2,4 +2,7 @@ import io from "socket.io-client";
 
 const url = window.location.href.split("/");
 
-export default io(`${url[0]}//${url[2]}`.replace("3000", "8600"));
+export default io(`${url[0]}//${url[2]}`.replace("3000", "8600"), {
+  //@ts-ignore
+  withCredentials: true,
+});
